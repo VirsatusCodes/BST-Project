c83648899e8987cf6806d696a0e7a5171ebb80fc
Donavyn <donavynpence@yahoo.com>
2022-11-26 13:04:23 -0700

    Node made, pretty print incorporated, begin testing for sorting

--- tree.js
+++ tree.js
@@ -1,22 +1,40 @@
 export{Node, Tree};
 
+
+
 const Node = (inp) => {
     let data = inp || null;
-    let left = 'filler';
-    let right = 'filler';
+    let left = null;
+    let right = null;
     const getData = () => data;
     const getLeft = () => left;
     const getRight = () => right;
-    const setLeft = () => {
-        'do what?'
-    }
-    const setRight = () => {
-        'and what else?'
-    }
+    const setLeft = (inp) => left = inp;
+    const setRight = (inp) => right = inp;
 
-    return {getData, getLeft, getRight}
+    return {getData, getLeft, getRight, setLeft, setRight}
 }
 
-const Tree = () => {
+/* will first test with a sorted array with no dupes, then incorporate
+unsorted arrays after. */
 
+const Tree = (arr) => {
+
+const root = buildTree();
+
+const buildTree = () => {
+
+}
+
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+    if (node.getRight() !== null) {
+      prettyPrint(node.getRight(), `${prefix}${isLeft ? '│   ' : '    '}`, false);
+    }
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+    if (node.getLeft() !== null) {
+      prettyPrint(node.getLeft(), `${prefix}${isLeft ? '    ' : '│   '}`, true);
+    }
+  }
+
+return {prettyPrint, root}
 }
--- tree.test.js
+++ tree.test.js
@@ -1,6 +1,23 @@
 import {Node, Tree} from '../BST-Project/tree';
 
+const testNode = Node('Wow it works');
+const testTree = Tree([1,2,3,4,5,6,7]);
+
 test('linked properly', () => {
     expect(Node);
     expect(Tree);
 });
+
+test('creates a node with an accessible value', () => {
+    expect(testNode.getData()).toBe('Wow it works');
+});
+
+test('accurately chooses its root node', () => {
+    expect(testTree.getLeft()).toBe(2);
+});
+
+
+/* test('accurately chooses its left and right value', () => {
+    expect(testTree.getLeft()).toBe(2);
+    expect(testTree.getLeft()).toBe(6);
+}); */
