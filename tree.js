1f5e255e706c6265b687f66e60e0df8aa76e67e0
Donavyn <donavynpence@yahoo.com>
2022-11-26 12:27:45 -0700

    files made and linked, jest working

---
+++ tree.js
@@ -0,0 +1,22 @@
+export{Node, Tree};
+
+const Node = (inp) => {
+    let data = inp || null;
+    let left = 'filler';
+    let right = 'filler';
+    const getData = () => data;
+    const getLeft = () => left;
+    const getRight = () => right;
+    const setLeft = () => {
+        'do what?'
+    }
+    const setRight = () => {
+        'and what else?'
+    }
+
+    return {getData, getLeft, getRight}
+}
+
+const Tree = () => {
+
+}
---
+++ tree.test.js
@@ -0,0 +1,6 @@
+import {Node, Tree} from '../BST-Project/tree';
+
+test('linked properly', () => {
+    expect(Node);
+    expect(Tree);
+});
