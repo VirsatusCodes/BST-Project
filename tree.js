5cb8b8bb8ff7069bae69127f14480138b4603d12
Donavyn <donavynpence@yahoo.com>
2022-12-03 12:43:45 -0700

    automatic sorting and duplicate removal added and tested

--- tree.js
+++ tree.js
@@ -1,35 +1,80 @@
 export{Node, Tree};
 
-
-
 const Node = (inp) => {
-    let data = inp || null;
-    let left = null;
-    let right = null;
-    const getData = () => data;
-    const getLeft = () => left;
-    const getRight = () => right;
-    const setLeft = (inp) => left = inp;
-    const setRight = (inp) => right = inp;
+  let data = inp || null;
+  let left = null;
+  let right = null;
+  const getData = () => data;
+  const getLeft = () => left;
+  const getRight = () => right;
+  const setLeft = (inp) => left = inp;
+  const setRight = (inp) => right = inp;
 
-    return {getData, getLeft, getRight, setLeft, setRight}
+  return {getData, getLeft, getRight, setLeft, setRight}
 }
-
-/* will first test with a sorted array with no dupes, then incorporate
-unsorted arrays after. */
 
 const Tree = (arr) => {
 
+const removeDuplicates = (arr) => {
+
+  const checkValue = (sorted) => {
+    return sorted != arr[counter]
+  }
+
+  const sorted = [];
+  let counter = 0
+
+  while(counter < arr.length) {
+    if(sorted.every(checkValue)) {
+      sorted.push(arr[counter]);
+    }
+    counter++;
+  }
+  return sorted
+}
+
+const mergeSort = (arr) => {
+  if (arr.length === 0) return "Please provide a non empty array!"
+  if (arr.length === 1) return arr;
+  const mid = Math.floor(arr.length / 2);
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid, arr.length);
+  return merge(mergeSort(left), mergeSort(right));
+};
+
+const merge = (leftArr, rightArr) => {
+  const result = [];
+  let iL = 0;
+  let iR = 0;
+  while (iL < leftArr.length && iR < rightArr.length) {
+    if (leftArr[iL] < rightArr[iR]) {
+      result.push(leftArr[iL]);
+      iL++;
+    } else {
+      result.push(rightArr[iR]);
+      iR++;
+    }
+  }
+  while (iL < leftArr.length) {
+    result.push(leftArr[iL]);
+    iL++;
+  }
+  while (iR < rightArr.length) {
+    result.push(rightArr[iR]);
+    iR++;
+  }
+  return result;
+};
 
 const buildTree = (arr) => {
 
 if (arr.length === 0) {
-  return null
+return null
 }
- 
+
 if(arr.length === 1) {
-  const lastNode = Node(arr[0]);
-  return lastNode;
+const lastNode = Node(arr[0]);
+return lastNode;
 }
 const mid = Math.floor(arr.length/2);
 const left = arr.slice(0, mid);
@@ -45,19 +90,22 @@ newNode.setRight(buildTree(right));
 return newNode
 
 }
-const root = buildTree(arr);
+const root = buildTree(mergeSort(removeDuplicates(arr)));
 
 const getRoot = () => root;
 
 const prettyPrint = (node, prefix = '', isLeft = true) => {
-    if (node.getRight() !== null) {
-      prettyPrint(node.getRight(), `${prefix}${isLeft ? '│   ' : '    '}`, false);
-    }
-    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.getData()}`);
-    if (node.getLeft() !== null) {
-      prettyPrint(node.getLeft(), `${prefix}${isLeft ? '    ' : '│   '}`, true);
-    }
+  if (node.getRight() !== null) {
+    prettyPrint(node.getRight(), `${prefix}${isLeft ? '│   ' : '    '}`, false);
   }
+  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.getData()}`);
+  if (node.getLeft() !== null) {
+    prettyPrint(node.getLeft(), `${prefix}${isLeft ? '    ' : '│   '}`, true);
+  }
+}
 
-return {prettyPrint, getRoot}
+return {prettyPrint,
+        getRoot, 
+        removeDuplicates,
+        mergeSort}
 }
--- tree.test.js
+++ tree.test.js
@@ -1,26 +1,23 @@
 import {Node, Tree} from '../BST-Project/tree';
 
-const testNode = Node('Wow it works');
 const oneTree = Tree([1,2,3]);
 const twoTree = Tree([1,2]);
-const threeTree = Tree([1,2,3,4,5,6,7]);
-const fourTree = Tree([1,2,3,4,5,6,7,8]);
-fourTree.prettyPrint(fourTree.getRoot());
+const threeTree = Tree([1,2,3,4,5,6,7,8,1,2,3,4,5,4,3,4,5,6,7,8]);
 
 test('linked properly', () => {
     expect(Node);
     expect(Tree);
 });
 
-test('creates a node with an accessible value', () => {
-    expect(testNode.getData()).toBe('Wow it works');
+test('remove duplicate values', () => {
+    expect(oneTree.removeDuplicates([1,2,3,3,4])).toStrictEqual([1,2,3,4])
 });
 
-test('accurately chooses its root node', () => {
-    expect(oneTree.getRoot().getData()).toBe(2);
+test('mergeSort works', () => {
+    expect(oneTree.mergeSort([2,3,4,7,4,1])).toStrictEqual([1,2,3,4,4,7])
 });
 
-test('accurately chooses its left and right value', () => {
+test('can make a tree with 3 nodes', () => {
     expect(oneTree.getRoot().getLeft().getData()).toBe(1);
     expect(oneTree.getRoot().getRight().getData()).toBe(3);
 });
@@ -29,17 +26,10 @@ test('can make a tree with 2 nodes', () => {
     expect(twoTree.getRoot().getData()).toBe(2);
     expect(twoTree.getRoot().getLeft().getData()).toBe(1);
     expect(twoTree.getRoot().getRight()).toBe(null)
-})
-
-test('works on bigger trees(7)', () => {
-    expect(threeTree.getRoot().getData()).toBe(4);
-    expect(threeTree.getRoot().getLeft().getData()).toBe(2);
-    expect(threeTree.getRoot().getLeft().getLeft().getData()).toBe(1);
-    expect(threeTree.getRoot().getRight().getData()).toBe(6);
-})
+});
 
 test('works on bigger trees(8)', () => {
-    expect(fourTree.getRoot().getData()).toBe(5);
-    expect(fourTree.getRoot().getLeft().getRight().getData()).toBe(4);
-    expect(fourTree.getRoot().getLeft().getRight().getRight()).toBe(null);
-})
+    expect(threeTree.getRoot().getData()).toBe(5);
+    expect(threeTree.getRoot().getLeft().getRight().getData()).toBe(4);
+    expect(threeTree.getRoot().getLeft().getRight().getRight()).toBe(null);
+});
